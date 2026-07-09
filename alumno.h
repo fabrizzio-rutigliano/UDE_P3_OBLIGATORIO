296a2279bbf0c78284850a80a8404c6ff4792f04
Fabrizzio Rutigliano <fabrizzio.rutigliano@gdn.com.uy>
2026-07-08 21:55:30 -0300

    Primeros cabezales y definiciones de trabajo

---
+++ alumno.h
@@ -0,0 +1,8 @@
+/*typedef struct Alumno {
+                	int ci;
+                	string nombre;
+                	string apellido;
+                	string telefono;
+                	Escolaridad escolaridad;
+};
+*/
---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+/*
+
+Fecha String arreglar boolean, y limpiar no utilizables (Gus)
+
+Alumno  GP
+Alumnos GP
+Asignatura Gus
+Asignaturas Gus
+Curricula Fabri
+Constantes GP
+
+*/
