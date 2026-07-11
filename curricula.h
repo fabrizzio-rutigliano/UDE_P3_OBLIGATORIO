c947a955e85d0cc0fbfd71938420e3f1336c8426
Fabrizzio Rutigliano <fabrizzio.rutigliano@gdn.com.uy>
2026-07-11 11:48:03 -0300

    Hedares de curricula y letra del obligatorio

--- curricula.h
+++ curricula.h
@@ -2,5 +2,36 @@
 #define CURRICULA_H_INCLUDED
 
 #include "asignaturas.h"
+#include "constantes.h"
+
+typedef int MatrizPreviaturas[MAX_ASIG][MAX_ASIG];
+
+// Crea un grafo vacío (Inicializa toda la matriz de adyacencia en 0).
+void Crear(MatrizPreviaturas &matriz);
+
+// Determina si en el grafo existe la arista especificada.
+bool PerteneceArista(MatrizPreviaturas matriz, int previa, int requiere);
+
+// Inserta la nueva arista al grafo.
+// Precondición: la arista no pertenece al grafo. 
+void InsertarArista(MatrizPreviaturas &matriz, int previa, int requiere);
+
+// Determina si en el grafo existe el vertice especificado.
+bool PerteneceVertice(MatrizPreviaturas matriz, int previa, int requiere);
+
+// Devuelve el grado del vértice especificado.
+// Precondición: el vértice pertenece al grafo.
+int GradoVertice (MatrizPreviaturas matriz, int vertice, int topeActual);
+
+// ==========================================
+// Operaciones especificas
+// ==========================================
+
+// Requerimiento 2: Determina si agregar la arista formaría un ciclo cerrado
+bool GeneraCiclo(MatrizPreviaturas matriz, int previa, int requiere, int topeActual);
+
+// Requerimiento 6: Lista por pantalla los números de todas las materias previas 
+//(directas e indirectas) de un vértice dado.
+void ListarTodasLasPrevias(MatrizPreviaturas matriz, int numAsig, int topeActual);
 
 #endif// CURRICULA_H_INCLUDED
